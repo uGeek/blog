6041a59c579559c962ebafa3a0ba72dbdc6874cd
ugeek <ugeekpodcast@gmail.com>
2021-08-09 19:49:20 +0200

    update

--- content_blog.js
+++ content_blog.js
@@ -1,5 +1,11 @@
 var tipuesearch = {pages: [
 {
+            'title': 'Lista de repositorios de Jellyfin.',
+            'text': 'En las últimas versiones de Jellyfin, se incorporó la posibilidad de añadir plugins mediante repositorios.', 
+            'tags': 'jellyfin,multimedia,,,,', 
+            'url': 'https://ugeek.github.io/blog/post/2021-08-09-lista-de-repositorios-de-jellyfin-.html' 
+},
+{
             'title': 'Editando CSV usando Emacs',
             'text': 'Edito muchos archivos CSV (valores separados por comas) y aunque puedo hacerlo con un programa de hoja de cálculo, me resulta más rápido hacerlo con Emacs.', 
             'tags': 'emacs,csv,,,,', 
@@ -883,7 +889,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'html2txt. Convirtiendo una web a texto plano con w3m',
-            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo -.html org-bash-blog.html org-bash-podcast.html a texto plano.', 
+            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo index.html list.html org-bash-blog.html tag.html a texto plano.', 
             'tags': 'w3m,bash,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2020-01-13-html2txt-convirtiendo-una-web-a-texto-plano-con-w3m.html' 
 },
