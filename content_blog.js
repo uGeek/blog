77d948c668935ebc5c12d5158ab959e20231d588
uGeek <ugeekpodact@gmail.com>
2023-01-07 14:04:28 +0100

    update

--- content_blog.js
+++ content_blog.js
@@ -7,7 +7,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'TigerVNC. Servidor VNC en Manjaro',
-            'text': '#+HTML: <center>', 
+            'text': 'TigerVNC es uno de los servidores que más me gusta para conectarme a mis escritorios remotos', 
             'tags': 'vnc,tigervnc,manjaro,lxde,i3wm,resolución', 
             'url': 'https://ugeek.github.io/blog/post/2023-01-06-tigervnc-servidor-vnc-en-manjaro.html' 
 },
@@ -1399,7 +1399,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'html2txt. Convirtiendo una web a texto plano con w3m',
-            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo org-bash-blog.html org-bash-podcast.html a texto plano.', 
+            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo org-bash-blog.html a texto plano.', 
             'tags': 'w3m,bash,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2020-01-13-html2txt-convirtiendo-una-web-a-texto-plano-con-w3m.html' 
 },
