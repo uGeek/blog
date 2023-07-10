da615bba5b13e7500cfe6964f0137c34f76b13f9
uGeek <ugeekpodact@gmail.com>
2023-07-10 22:07:44 +0200

    update

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 var tipuesearch = {pages: [
 {
+            'title': 'Firefox. Reducir el tamaño de las pestañas como en Brave, quitar marcadores y cambiar el tema',
+            'text': 'Estoy utilizando Firefox nuevamente, pero después de estar utilizando *Brave*, veo las pestañas muy grandes. Vamos a ver como reducir el tamaño de las pestañas y dejarlas aproximadamente como *Brave*', 
+            'tags': 'firefox,navegador,,,,', 
+            'url': 'https://ugeek.github.io/blog/post/2023-07-10-firefox-reducir-el-tamano-de-las-pestanas-como-en-brave-quitar-marcadores-y-cambiar-el-tema.html' 
+},
+{
             'title': 'SilverBullet. Mi página Índice',
             'text': 'Con SilverBullet puedes crearte una buena página índice con muchísimas opciones e información.', 
             'tags': 'silverbullet,notas,markdown,,,', 
@@ -1687,7 +1693,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'html2txt. Convirtiendo una web a texto plano con w3m',
-            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo -.html org-bash-blog.html org-bash-podcast.html a texto plano.', 
+            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo index.html list.html org-bash-blog.html tag.html a texto plano.', 
             'tags': 'w3m,bash,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2020-01-13-html2txt-convirtiendo-una-web-a-texto-plano-con-w3m.html' 
 },
@@ -2341,7 +2347,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'Papelera Reciclaje desde la Terminal',
-            'text': 'Cuando utilizamos cualquier tipo de escritorio con interfaz gráfica y borramos un archivo, como sabéis, este archivo en realidad no ha sido borrado, sino que ha sido movido a la papelera. Esto no sucede así cuando utilizamos nuestra terminal. Cuando borras el archivo desde la terminal, lo borras para siempre. Hoy os traigo una herramienta que en lugar de hacer go1.20.3.linux-arm64.tar.gz y eliminarlo para siempre, nos va a llevar este archivo a la papelera. Así mismo, mediante una serie de comandos, nos va a permitir el gestionar todo el contenido de nuestra papelera desde la terminal.', 
+            'text': 'Cuando utilizamos cualquier tipo de escritorio con interfaz gráfica y borramos un archivo, como sabéis, este archivo en realidad no ha sido borrado, sino que ha sido movido a la papelera. Esto no sucede así cuando utilizamos nuestra terminal. Cuando borras el archivo desde la terminal, lo borras para siempre. Hoy os traigo una herramienta que en lugar de hacer *rm* y eliminarlo para siempre, nos va a llevar este archivo a la papelera. Así mismo, mediante una serie de comandos, nos va a permitir el gestionar todo el contenido de nuestra papelera desde la terminal.', 
             'tags': 'bash,terminal,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2019-02-07-papelera-reciclaje-desde-la-terminal.html' 
 },
