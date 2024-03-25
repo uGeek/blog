518a5ce902348efda220844084701c1675de0aff
uGeek <ugeekpodact@gmail.com>
2024-03-25 12:07:05 +0100

    update

--- content.js
+++ content.js
@@ -6,6 +6,12 @@ var tipuesearch = {pages: [
             'url': 'https://ugeek.github.io/blog/post/2024-03-25-docker-merino-un-servidor-proxy-socks5-en-rust.html' 
 },
 {
+            'title': 'RSS. Google News, generando Feeds de temas personalizados',
+            'text': 'Google News es una herramienta de agregación de noticias desarrollada por Google. Esta plataforma recopila artículos de noticias de diversas fuentes y los presenta al usuario en función de sus', 
+            'tags': 'rss,google,news,feed,,', 
+            'url': 'https://ugeek.github.io/blog/post/2024-03-25-rss-google-news-generando-feeds-de-temas-personalizados.html' 
+},
+{
             'title': 'Android. Descargando vídeos de YouTube como podcast',
             'text': 'Quizás no sea tan importante generar un *Feed* de podcast para consumir los audios de YouTube. Con estas aplicaciones puedes escuchar o ver videos de todos los sitios web complatibles', 
             'tags': 'android,youtube,,,,', 
@@ -1771,7 +1777,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'html2txt. Convirtiendo una web a texto plano con w3m',
-            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo -.html org-bash-blog.html org-bash-podcast.html a texto plano.', 
+            'text': 'Ya hemos aprendido a utilizar w3m en Emacs y desde la terminal. Ahora vamos a convertir cualquier web o archivo index.html list.html org-bash-blog.html tag.html a texto plano.', 
             'tags': 'w3m,bash,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2020-01-13-html2txt-convirtiendo-una-web-a-texto-plano-con-w3m.html' 
 },
@@ -2425,7 +2431,7 @@ var tipuesearch = {pages: [
 },
 {
             'title': 'Papelera Reciclaje desde la Terminal',
-            'text': 'Cuando utilizamos cualquier tipo de escritorio con interfaz gráfica y borramos un archivo, como sabéis, este archivo en realidad no ha sido borrado, sino que ha sido movido a la papelera. Esto no sucede así cuando utilizamos nuestra terminal. Cuando borras el archivo desde la terminal, lo borras para siempre. Hoy os traigo una herramienta que en lugar de hacer go1.20.3.linux-arm64.tar.gz y eliminarlo para siempre, nos va a llevar este archivo a la papelera. Así mismo, mediante una serie de comandos, nos va a permitir el gestionar todo el contenido de nuestra papelera desde la terminal.', 
+            'text': 'Cuando utilizamos cualquier tipo de escritorio con interfaz gráfica y borramos un archivo, como sabéis, este archivo en realidad no ha sido borrado, sino que ha sido movido a la papelera. Esto no sucede así cuando utilizamos nuestra terminal. Cuando borras el archivo desde la terminal, lo borras para siempre. Hoy os traigo una herramienta que en lugar de hacer *rm* y eliminarlo para siempre, nos va a llevar este archivo a la papelera. Así mismo, mediante una serie de comandos, nos va a permitir el gestionar todo el contenido de nuestra papelera desde la terminal.', 
             'tags': 'bash,terminal,zsh,,,', 
             'url': 'https://ugeek.github.io/blog/post/2019-02-07-papelera-reciclaje-desde-la-terminal.html' 
 },
